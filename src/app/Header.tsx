a6c7029c9a2c02dc063cf619972c60ca27750916
Vijayrajdev <vijayrajck@outlook.com>
2023-05-27 20:26:43 +0530

    Navbar: Navlinks added

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import NavLinks from "./NavLinks";
 
 const Header = () => {
   return (
@@ -23,6 +24,9 @@ const Header = () => {
           </button>
         </div>
       </div>
+      {/* NavLinks */}
+      <NavLinks />
+      {/* SearchBox */}
     </header>
   );
 };
